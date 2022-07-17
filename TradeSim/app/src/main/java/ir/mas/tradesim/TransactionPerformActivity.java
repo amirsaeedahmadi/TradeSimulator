7780adb935c8fcfc0edf6ee264e9be0698e960e8
Mahdi Teymoori Anar <mahditeymoorianar@gmail.com>
2022-07-17 10:33:25 +0430

    TransactionPerformActivity GUI

--- TradeSim/app/src/main/java/ir/mas/tradesim/TransactionPerformActivity.java
+++ TradeSim/app/src/main/java/ir/mas/tradesim/TransactionPerformActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import ir.mas.tradesim.Model.Currency;
 import ir.mas.tradesim.Model.TransactionType;
@@ -12,6 +16,10 @@ public class TransactionPerformActivity extends AppCompatActivity {
     TransactionType transactionType;
     Currency currency;
     Intent intent;
+    TextView codeView, typeTextView;
+    ImageView typeImageView, currencyImageView;
+    EditText currencyView, rialView;
+    Button performButton, cancelButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +28,15 @@ public class TransactionPerformActivity extends AppCompatActivity {
         intent = getIntent();
         transactionType = (TransactionType) intent.getSerializableExtra("type");
         currency = Currency.getCurrencyByCode(intent.getStringExtra("code"));
+        // getting the elements
+        codeView = findViewById(R.id.codeTextView);
+        typeTextView = findViewById(R.id.typeTextView);
+        typeImageView = findViewById(R.id.typeImageView);
+        currencyImageView = findViewById(R.id.currencyLogoView);
+        currencyView = findViewById(R.id.CurrencyEditTextNumber);
+        rialView = findViewById(R.id.RialEditTextNumber);
+        performButton = findViewById(R.id.performButton);
+        cancelButton = findViewById(R.id.cancelButton);
         //
     }
 }
